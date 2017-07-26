a9322126d5e775782c11e2b98e025d961d4ac4d6
liyu <liyu@PN-datang-ly.local>
2017-07-26 17:47:35 +0800

    config web component

--- src/spittr/config/WebConfig.java
+++ src/spittr/config/WebConfig.java
@@ -1,7 +1,33 @@
 package spittr.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /**
  * Created by liyu on 2017/7/25.
  */
-public class WebConfig {
+@Configuration
+@EnableWebMvc //启用spring mvc
+@ComponentScan("spittr.web")
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Bean //配置视图解析器
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Override //对静态资源的请求转发到默认的servlet,而不是DIspatcherServlet
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();;
+    }
 }
