b60a87992ba879dc82576c66892110f06c4ce09c
liyu <liyu@PN-datang-ly.local>
2017-07-26 17:54:26 +0800

    config component not related to web

--- src/spittr/config/RootConfig.java
+++ src/spittr/config/RootConfig.java
@@ -1,7 +1,15 @@
 package spittr.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * Created by liyu on 2017/7/25.
  */
+@Configuration
+@ComponentScan(basePackages = {"spittr"},
+        excludeFilters = {@ComponentScan.Filter(type= FilterType.ANNOTATION, value = EnableWebMvc.class)})
 public class RootConfig {
 }
